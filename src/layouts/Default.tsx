0a443447166900b79c1e6db6d3bbd765843a6bd4
Natan Foleto <natan@imobpower.com>
2022-10-19 00:51:19 -0300

    Add footer in layout default

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 import styles from "./Default.module.css";
 
@@ -14,6 +15,8 @@ export function DefaultLayout({ children }: DefaultLayoutProps) {
       <Header />
 
       <div className={styles.container}>{children}</div>
+
+      <Footer />
     </div>
   );
 }
